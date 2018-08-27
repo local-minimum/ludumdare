07a2d75dd3351607f7d5cd38120bc2fdafed4d70
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-08-27 08:31:10 +0200

    Added restrictions on event mode for when my grades shows

--- src/com/page/nav/event.js
+++ src/com/page/nav/event.js
@@ -30,7 +30,7 @@ export default class PageNavEvent extends Component {
 		NavButtons.push(<ContentNavButton path={path+FirstPath} icon="ticket" href={path+'/theme'}>Theme</ContentNavButton>);
 		NavButtons.push(<ContentNavButton path={path+FullPath} icon="stats" href={path+'/stats'}>Stats</ContentNavButton>);
 
-		if ( IsLoggedIn ) {
+		if ( IsLoggedIn && ThemeMode >= 6 ) {
 			/* light={FirstPath == '/my'} */
 			NavButtons.push(<ContentNavButton path={path+FullPath} icon="star-half" href={path+'/my/grades'}>My Grades</ContentNavButton>);
 		}
